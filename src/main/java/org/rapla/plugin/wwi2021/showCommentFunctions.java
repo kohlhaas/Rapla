6a526f6cc646eb4b83ba8e2bae2cb8d9aa181f51
Liselotte Lichtenstein <wi21015@lehre.dhbw-stuttgart.de>
2024-06-26 16:01:17 +0200

    init comment functions

---
+++ src/main/java/org/rapla/plugin/wwi2021/showCommentFunctions.java
@@ -0,0 +1,105 @@
+package org.rapla.plugin.wwi2021;
+
+
+import org.rapla.entities.IllegalAnnotationException;
+import org.rapla.entities.User;
+import org.rapla.entities.domain.Appointment;
+import org.rapla.entities.domain.AppointmentBlock;
+import org.rapla.entities.domain.Reservation;
+import org.rapla.entities.dynamictype.Classifiable;
+import org.rapla.entities.dynamictype.Classification;
+import org.rapla.entities.dynamictype.internal.EvalContext;
+import org.rapla.entities.extensionpoints.Function;
+import org.rapla.entities.extensionpoints.FunctionFactory;
+import org.rapla.inject.Extension;
+
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.List;
+
+@Extension(provides = FunctionFactory.class, id=showCommentFunctions.NAMESPACE)
+
+
+public class showCommentFunctions implements FunctionFactory{
+
+
+    static final public String NAMESPACE = "org.rapla.showcomment";
+
+    public @Inject showCommentFunctions(){
+
+    }
+
+
+    @Override public Function createFunction(String functionName, List<Function> args) throws IllegalAnnotationException
+    {
+        if ( functionName.equals(CommentFunction.name))
+        {
+            return new CommentFunction(args);
+        }
+        return null;
+    }
+
+    private String showComment(Object obj)
+    {
+        final String l;
+
+        if ( obj instanceof AppointmentBlock)
+        {
+            l = "not supported Block";
+        }
+        else if (obj instanceof Appointment)
+        {
+            l= ((Appointment)obj).getComment();
+        }
+        else if (obj instanceof Reservation)
+        {
+            l = "not supported Reservation";
+        }
+        else if ( obj instanceof Collection)
+        {
+            l = "not supported Collection";
+        }
+        else
+        {
+            l = "nope";
+        }
+        return l;
+    }
+
+    class CommentFunction extends Function
+    {
+        static public final String name = "comment";
+        Function arg;
+
+        public CommentFunction( List<Function> args) throws IllegalAnnotationException
+        {
+            super(NAMESPACE,name, args);
+            assertArgs(0,1);
+            if ( args.size() > 0)
+            {
+                arg = args.get( 0);
+            }
+
+        }
+
+        @Override public String eval(EvalContext context)
+        {
+            final Object obj;
+            if ( arg != null)
+            {
+                obj = arg.eval( context);
+            }
+            else
+            {
+                obj = context.getFirstContextObject();
+            }
+            final String result = showComment(obj);
+            return result;
+        }
+
+    }
+
+}
+
+
+
